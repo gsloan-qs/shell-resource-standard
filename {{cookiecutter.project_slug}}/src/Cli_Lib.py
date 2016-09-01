138e1d6ea55941cf6b0c9d0de2b55e79221dd7ea
greg-s <gregory.s@qualisystems.com>
2016-09-01 08:14:18 -0700

    Update Cli_lib to handle non-default ports

--- {{cookiecutter.project_slug}}/src/Cli_Lib.py
+++ {{cookiecutter.project_slug}}/src/Cli_Lib.py
@@ -1,9 +1,13 @@
+import Exscript
+from Exscript.protocols import SSH2, Telnet
 import telnetlib
 import paramiko
 from cloudshell.core.logger import qs_logger
 import time
 import re
 re.match
+
+
 
 class Cli:
     handler = None
@@ -20,211 +24,78 @@ class Cli:
         :param logger:
         :return:
         """
-
+        self.account = Exscript.Account(username,password)
+        self.default_prompt_list = ['.*#', '.*>']
         if protocol.lower() == 'telnet':
             try:
-                self.handler = TelnetHandler(address, port, username, password, timeout, logger)
+
+                self.handler = Telnet()
+                self.handler.set_prompt(self.default_prompt_list)
+                self.handler.connect(address, port)
+
+                self.handler.login(self.account)
+
             except:
                 raise IOError('Unable to connect to device')
 
         elif protocol.lower() == 'ssh':
             try:
-                self.handler = SSHHandler(address, port, username, password, timeout, logger)
+                self.handler = SSH2()
+                self.handler.set_prompt(self.default_prompt_list)
+                self.handler.connect(address, port)
+                self.handler.login(self.account)
             except:
                 raise IOError('Unable to connect to device')
 
         elif protocol.lower() == 'auto':
             try:
-                self.handler = SSHHandler(address, port, username, password, timeout, logger)
+                self.handler = SSH2()
+                self.handler.set_prompt(self.default_prompt_list)
+                self.handler.connect(address, port)
+                self.handler.login(self.account)
             except:
                 try:
-                    self.handler = TelnetHandler(address, port, username, password, timeout, logger)
+                    self.handler = Telnet()
+                    self.handler.set_prompt(self.default_prompt_list)
+                    self.handler.connect(address, port)
+                    self.handler.login(self.account)
                 except:
                     raise IOError('Unable to connect to device')
         else:
             raise AttributeError('invalid protocol')
 
+    def send_and_receive(self, command, pattern_list=None):
 
-class SSHHandler(paramiko.SSHClient):
-    def __init__(self, address, port=22, username='', password='', timeout=60, logger=None):
-
-            if logger is None:
-                logger = qs_logger.get_qs_logger('Provision Telnet')
-            self.logger = logger
-
-            logger.info('Initialize SSHHandler: {address=' + address + ' port=' + str(port) + ' timeout=' +
-                        str(timeout))
-
-            paramiko.SSHClient.__init__(self)
-
-            self.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-            self.address = address
-            self.username = username
-            self.password = password
-            self.timeout = timeout
-            self.port = port
-
-            self.login()
-
-    def login(self):
-        self.logger.info('Logging in ssh handler')
-        self.connect(self.address, username=self.username, password=self.password, timeout=self.timeout,
-                     port=self.port)
-
-    def send_and_receive(self, line):
-        return self.exec_command(line)
-
-
-class TelnetHandler(telnetlib.Telnet):
-
-    login_prompt = '[Ll]ogin:'
-    password_prompt = '[Pp]assword:'
-    default_prompt_set = ['.*?#', '.*?>', '.*?%', login_prompt, password_prompt]
-    last_result = ''
-    last_result_stripped = ''
-    last_pattern_index = -1
-
-    def __init__(self, address, port=23, username='', password='', timeout=60, logger=None):
-
-            if logger is None:
-                logger = qs_logger.get_qs_logger('TelnetHandler')
-
-            self.logger = logger
-
-            self.logger.info('Initialize TelnetHandler: {address=' + address + ' port=' + str(port) + ' timeout=' +
-                        str(timeout))
-            if timeout is 0:
-                telnetlib.Telnet.__init__(self, address, port)
-            else:
-                telnetlib.Telnet.__init__(self, address, port, timeout)
-
-            self.address = address
-            self.port = port
-            self.timeout=timeout
-            self.username = username
-            self.password = password
-
-            #TODO Move login to wrapper init
-            self.login()
-
-    def login(self):
-
-        self.logger.info('Logging in telnet handler')
-        self.send_and_receive('\r\n', use_pattern=True)
-        #TODO clarify pattern matching without using indexes
-        if self.last_pattern_index == self.default_prompt_set.index(self.login_prompt):  # Matched Login pattern
-            if self._from_login_prompt() == -1:
-                return -1
-
-        elif self.last_pattern_index == 4: # Started at password prompt
-            self.send_and_receive('\r\n')
-            if self.last_pattern_index == 3: # Matched Login pattern
-                if self._from_login_prompt() == -1:
-                    return -1
-
-        elif self.last_pattern_index in [0,1,2]: # Already logged in
-            while self.last_pattern_index not in [-1,3]:
-                self.send_and_receive('exit')
-
-            if self.last_pattern_index == 3:
-                self._from_login_prompt()
-
-            else:
-                self.open(self.address, self.port, self.timeout)
-                self.send_and_receive('\r\n')
-                if self.last_pattern_index == 3:
-                    self._from_login_prompt()
-                elif self.last_pattern_index == -1:
-                    self.logger.error('Incorrect login result, unable get prompt')
-                    self.logger.debug(self.last_result_stripped)
-
-        self.logger.info('Successful login')
-        return 1
-
-
-    def _from_login_prompt(self):
-        self.send_and_receive(self.username)
-        if self.last_pattern_index == 4: # Matched Password pattern
-            self.send_and_receive(self.password)
-            if self.last_pattern_index not in [0,1,2]: # Did not receive prompt
-                self.logger.error('Incorrect login result, no matching prompt')
-                self.logger.debug(self.last_result_stripped)
-                return -1
-        else: # Did not get password prompt
-            self.logger.error('Incorrect login result, did not receive password prompt')
-            self.logger.debug(self.last_result_stripped)
-            return -1
-
-        return 1
-
-    def strip_last_line(self, content):
-        try:
-            return content.rsplit('\r\n', 1)[0]
-        except IndexError:
-            return content
-
-    def strip_first_line(self, content):
-        try:
-            return content.split('\r\n', 1)[1]
-        except IndexError:
-            return content
-
-    def clear_read_buffer(self):
-        result = None
-
-        while result != '':
-            result = self.read_very_eager()
-
-    def send_and_receive(self, line, use_pattern=True, patterns=[]):
-        #TODO return the objects instead of setting internal members
-        self.logger.info('sending "' + line + '" via telnet Handler')
-        self.clear_read_buffer()
-        self.last_result = ''
-        if '\r\n' not in line:
-            line += '\r\n'
-
-        self.write(line)
-        self.logger.info('Line sent')
-        if not use_pattern:
-            self.logger.info('Reading very eager')
-            result = ''
-            temp_result = None
-            time.sleep(2)
-            while temp_result != '':
-
-                temp_result = self.read_very_eager()
-                result += temp_result
-
-                # result = self.read_very_eager()
-                self.last_result_stripped = self.strip_first_line(self.strip_last_line(result))
-                self.logger.info('read complete')
-
+        if pattern_list:
+            if type(pattern_list) != list:
+                pattern_list = [pattern_list]
+            self.handler.set_prompt(pattern_list)
         else:
-            self.logger.info('expecting pattern set')
-            if len(patterns) == 0:
-                patterns = self.default_prompt_set
-            try:
-                result = self.expect(patterns)
-            except:
-                self.last_pattern_index = -1
-                try:
-                    self.logger.debug(result[2])
-                except:
-                    self.logger.debug('unable to log result')
-                return self.last_pattern_index, self.last_result, self.last_result_stripped
-            self.last_result = result[2]
-            self.last_result_stripped = self.strip_first_line(self.strip_last_line(result[2]))
-            self.last_pattern_index = result[0]
-            self.logger.info('Expect complete')
-            self.logger.info('Last Pattern Index: ' + str(self.last_pattern_index))
-        self.logger.debug(self.last_result)
-        self.logger.debug(self.last_result_stripped)
-        return self.last_pattern_index, self.last_result, self.last_result_stripped
+            pattern_list = self.default_prompt_list
+            self.handler.set_prompt(self.default_prompt_list)
+
+        resultcode, match = self.handler.execute(command)
+
+        return resultcode, pattern_list[resultcode], match.string # Pattern index, pattern, buffer
+
+    def expect(self, pattern_list=None):
+        if pattern_list:
+            if type(pattern_list) != list:
+                pattern_list = [pattern_list]
+        else:
+            pattern_list = self.default_prompt_list
+
+
+        returncode, match = self.handler.expect(pattern_list)
+
+        return returncode, pattern_list[returncode], match.string # Pattern index, pattern, buffer
+
 
 
 def __main__():
-    pass
+    session = Cli('localhost','23','greg-s', 'jgT777810', protocol = 'Telnet')
+    result = session.send_and_receive('hostname')
+    print result
 
 if __name__ == '__main__':
     __main__()
